a9bb36b3c5339128821c5f211d2cb7f591ffc649
ArkieSoft@Github <wyatt@arkannon.com>
2024-08-22 20:14:54 -0700

    Removed Scripts due to ass.nix

--- scripts/dance.sh
+++
@@ -1,5 +0,0 @@
-#!/run/current-system/sw/bin/bash
-
-play -q --replay-gain track /etc/nixos/sounds/driftveil.mp3&
-clear
-chafa --bg 181818 -C on --scale 2 /etc/nixos/assets/toothless.gif
--- scripts/hydro.sh
+++
@@ -1,8 +0,0 @@
-#!/run/current-system/sw/bin/bash
-
-while true; do
-   A=$((900 + RANDOM % 900))
-   play /etc/nixos/sounds/alert.mp3 &
-   dunstify -u critical -t 5000 "Hey, Hydrate you retard!"
-   sleep $A
-done
